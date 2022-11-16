b5cef3b7b9286e691fec5d4531738eda7cede287
Anoshko Alexey <anoshkoalexey@gmail.com>
2022-11-15 19:32:48 -0800

    Add enum RoomType.java

---
+++ src/model/RoomType.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ * Types of rooms
+ *
+ * @author Anoshko Alexey
+ *
+ */
+public enum RoomType {
+    /**
+     * Single room
+     */
+    SINGLE,
+    /**
+     * Double room
+     */
+    DOUBLE
+}
