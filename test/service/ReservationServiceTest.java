98258ac2d0fa3d9becdb879e2662460c2b063e82
Anoshko Alexey <anoshkoalexey@gmail.com>
2022-11-17 15:22:20 -0800

    Create the ReservationService class

---
+++ test/service/ReservationServiceTest.java
@@ -0,0 +1,33 @@
+//TODO implement the class
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationServiceTest {
+
+    @Test
+    void addRoom() {
+    }
+
+    @Test
+    void getARoom() {
+    }
+
+    @Test
+    void reserveARoom() {
+    }
+
+    @Test
+    void findRooms() {
+    }
+
+    @Test
+    void getCustomersReservation() {
+    }
+
+    @Test
+    void printAllReservation() {
+    }
+}
